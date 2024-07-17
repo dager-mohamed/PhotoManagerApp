0dd87e2c733fc215b9ecfd8048d53f177f2cbbb9
Dagermohamed <mohmed6002090@gmail.com>
2024-07-18 00:59:51 +0300

    removing adapters from auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,2 @@
-import { authOptions } from "@/auth";
-import NextAuth from "next-auth";
-
-export const { auth, handlers, signIn, signOut, unstable_update } =
-  NextAuth(authOptions);
-
+import { handlers } from "@/auth";
 export const { GET, POST } = handlers;
--- auth.ts
+++ auth.ts
@@ -1,9 +1,7 @@
-import NextAuth, { NextAuthConfig } from "next-auth";
+import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-import { FirestoreAdapter } from "@auth/firebase-adapter";
-import { cert } from "firebase-admin/app";
 
-export const authOptions: NextAuthConfig = {
+export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
@@ -11,19 +9,4 @@ export const authOptions: NextAuthConfig = {
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
-  adapter: FirestoreAdapter({
-    credential: cert({
-      projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
-      clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY,
-    }),
-  }),
-};
-
-export {
-  auth,
-  handlers,
-  signIn,
-  signOut,
-  unstable_update,
-} from "@/app/api/auth/[...nextauth]/route";
+});
